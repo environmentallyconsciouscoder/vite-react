70021a58678a49d66e4dc1a5c013c041a91e9940
environmentallyconsciouscoder <environmentallyconsciouscoder@gmail.com>
2025-02-28 10:55:43 +0000

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/chat': {
-        target: 'http://51.44.183.204', // Your original HTTP endpoint
-        changeOrigin: true,  // Modify the origin of the host header to the target
-        secure: false,       // Allows insecure requests from the API
-      },
-    },
-  },
 })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
